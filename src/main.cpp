5035d96c38e569a9f5177008ea3bea7de7a26ba1
Max <7087907+maxpromer@users.noreply.github.com>
2023-08-23 16:09:05 +0700

    Add show count on IOXESP32 4-Digit shield

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
 #include <Arduino.h>
 #include <IOXESP32Audio.h>
+#include <IOXESP32_4Digit.h>
 
 #define NEXT_SW_PIN (0)
 
 unsigned int count = 0;
 
 void setup() {
+  Serial.begin(115200);
+
   Audio.begin(); // Init IOXESP32 Audio shield V2
+  Display.begin(); // Init IOXESP32 4-Digit shield
 }
 
 void loop() {
-  if (digitalRead(NEXT_SW_PIN) == LOW) {
-    while(digitalRead(NEXT_SW_PIN) == LOW) delay(10);
+  if (digitalRead(NEXT_SW_PIN) == LOW) { // if switch are press
+    while(digitalRead(NEXT_SW_PIN) == LOW) delay(10); // wait switch up
     
     count++; // count up
+    Display.print(count); // show count on display
+    Display.blinkRate(2); // 0: Blink OFF, 1: Blink with 2 Hz, 2: Blink with 1 Hz, 3: Blink with 1/2 Hz
 
     Serial.print("call queue ");
     Serial.print(count);
@@ -75,6 +81,7 @@ void loop() {
     }
 
     Serial.println(")");
+    Display.blinkRate(0); // 0: Blink OFF
   }
 
   delay(10); // make sure other task work
